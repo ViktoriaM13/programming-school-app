6ea306474f26d770f4befaece67bbdf401d2f656
Victoria <victoria.m13@mail.ru>
2021-08-19 16:21:55 +0300

    'first-commit'

---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,20 @@
+const Courses = (props) => {
+
+    return(
+            <div>
+                <h2 className="h2 text-uppercase animate__animated animate__fadeInLeft">Краткосрочные курсы</h2>
+                <p className="animate__animated animate__fadeInRight">
+                    IT образование: учись
+                    у лучших, 100% практики
+                </p>
+                <p className="animate__animated animate__fadeInRight">
+                    Обучим только актуальным знаниям и навыкам,
+                    выдаем Сертификат или Диплом который котируется
+                    у работодателей
+                </p>
+                <h3 className="animate__animated animate__fadeInLeft">Каталог курсов</h3>
+            </div>
+    )
+}
+
+export default Courses;
